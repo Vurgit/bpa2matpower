7d8caeafe2edb401232455414173979b2b45b312
Vurgit <56619134+Vurgit@users.noreply.github.com>
2019-12-04 10:23:40 +0800

    Add files via upload
    English output and annotation added

--- bpa2matpower.m
+++ bpa2matpower.m
@@ -1,10 +1,14 @@
 % 读取BPA格式数据文件，生成MATPOWER所需电网对象和母线列表
+% Reading BPA file,generating grid and bus info for MATPOWER
 function [mpc, buslist] = bpa2matpower(bpafile)
 % 1. 读入BPA文件的所有内容
-disp(['读入原始BPA潮流文件', bpafile, '的内容...']);
+% 1.Reading BPA file
+disp(['读入原始BPA潮流文件', bpafile, '的内容...  ','  Reading original BPA powerflow file ',bpafile,'...']);
 % 1.1. 打开BPA潮流文件
+% 1.1 Opening BPApowerflow file
 fid = fopen(bpafile, 'r');
 % 1.2. 将所有内容读入到字符串集合中
+% 1.2  Reading all the content to a cell
 bpa_info = cell(5000, 1);
 ii = 1;
 while (~feof(fid))
@@ -13,18 +17,22 @@ while (~feof(fid))
     ii = ii + 1;
 end
 % 1.3. 将未利用到的预分配数组元素所占资源释放掉
+% 1.3. Releasing resources for unused cells
 for jj = ii : 5000
     bpa_info(ii) = [];
 end
 % 1.3. 关闭BPA潮流文件
+% 1.3. Closing BPA file
 fclose(fid);
 % 2. 生成MatPower电力网络对象和母线名称列表
+% 2. Generating BPA grid data and bus name list
 % 2.1. 得到母线个数，支路个数，基准容量
+% 2.1  Getting basic grid data（Number of buses and branches and baseMVA
 busNumber = 0;
 genNumber = 0;
 branchNumber = 0;
 baseMVA=100;
-disp('获得电网基本参数（母线个数，支路个数，基准容量）...');
+disp('获得电网基本参数（母线个数，支路个数，基准容量）...  Getting basic grid data（Number of buses and branches and baseMVA)...');
 for ii = 1 : length(bpa_info)
     thisline = bpa_info{ii};
     % 2.1.1. 获得基准容量
@@ -45,65 +53,74 @@ for ii = 1 : length(bpa_info)
 end
 % 2.2. 得到母线矩阵，发电机矩阵，支路矩阵，母线列表
 % 2.2.1. 得到母线矩阵和母线名称列表
+% 2.2 Getting bus matrix,generator matrix,branch matrix,bus list
 busMatrix = zeros(busNumber, 13);
 buslist = cell(busNumber, 1);
 busIndex = 0;
-disp('获得母线矩阵和母线名称列表...');
+disp('获得母线矩阵和母线名称列表...  Getting bus matrix and bus name list...');
 for ii = 1 : length(bpa_info)
     thisline = bpa_info{ii};
-    if (length(thisline) < 72)  % 在长度不足72的字符串后面补足空格
+    if (length(thisline) < 72)  % 在长度不足72的字符串后面补足空格 
+% Add space for char(string?) shorter than 72(One Chinese character is 1
+% space)
         thisline = [thisline, blanks(72 - length(thisline))];
     end
     if (strcmp(thisline(1), 'B'))
         busIndex = busIndex + 1;
-        busMatrix(busIndex, 1) = busIndex;  % 母线编号
-        switch thisline(2)  % 母线类型
+        busMatrix(busIndex, 1) = busIndex;  % 母线编号 Bus no
+        switch thisline(2)  % 母线类型 Bus type
             case {' ', 'T', 'C', 'V'}   % PQ节点
                 busMatrix(busIndex, 2) = 1;
             case {'E', 'Q', 'G'}    % PV节点
                 busMatrix(busIndex, 2) = 2;
-            case 'S'    % 平衡节点
+            case 'S'    % 平衡节点 Slack bus
                 busMatrix(busIndex, 2) = 3;
         end
         if (~strcmp(thisline(21 : 25), blanks(5)))
-            busMatrix(busIndex, 3) = str2double(thisline(21 : 25));   % 有功负荷
+            busMatrix(busIndex, 3) = str2double(thisline(21 : 25));   % 有功负荷 P load
         else
             busMatrix(busIndex, 3) = 0.0;   % 有功负荷
         end
         if (~strcmp(thisline(26 : 30), blanks(5)))
-            busMatrix(busIndex, 4) = str2double(thisline(26 : 30));   % 无功负荷
+            busMatrix(busIndex, 4) = str2double(thisline(26 : 30));   % 无功负荷 Q load
         else
             busMatrix(busIndex, 4) = 0.0;   % 无功负荷
         end
         if (~strcmp(thisline(31 : 34), blanks(4)))
             busMatrix(busIndex, 5) = str2double(thisline(31 : 34));   % 对地电导（用额定电压下有功来表示）
+                                                                      %Ground-conductance(P under rated voltage)                                                                                                                                            
         else
             busMatrix(busIndex, 5) = 0.0;   % 对地电导（用额定电压下有功来表示）
         end
         if (~strcmp(thisline(35 : 38), blanks(4)))
             busMatrix(busIndex, 6) = str2double(thisline(35 : 38));   % 对地电纳（用额定电压下无功来表示，容性为正）
+                                                                      %Ground susceptance(Q under rated voltage,capacitive-is-positive                                                                      
         else
             busMatrix(busIndex, 6) = 0.0;   % 对地电纳（用额定电压下无功来表示，容性为正）
         end
         busMatrix(busIndex, 7) = 1;   % 母线所在area索引（***这里暂时假定全网只有一个area，后面可改进）
+                                      %bus area(let it be all the same)
         if (~strcmp(thisline(58 : 61), blanks(4)))
             thisVoltage = str2double(thisline(58 : 61));
             while (thisVoltage > 2)
                 thisVoltage = thisVoltage / 10;
             end
             busMatrix(busIndex, 8) = thisVoltage;   % 母线电压值
+                                                    %bus voltage amplitude
         else
             busMatrix(busIndex, 8) = 1.0;   % 母线电压值
         end
         busMatrix(busIndex, 9) = 0.0; % 母线相角值
+                                      % bus angle 
         busMatrix(busIndex, 10) = str2double(thisline(15 : 18));  % 母线电压等级
+                                                                  % bus baseKV
         busMatrix(busIndex, 11) = 1;  % 母线所在zone索引（***这里暂时假定全网只有一个zone，后面可改进）
         if (~strcmp(thisline(58 : 61), blanks(4)))
             thisVoltageMax = str2double(thisline(58 : 61));
             while (thisVoltageMax > 2)
                 thisVoltageMax = thisVoltageMax / 10;
             end
-            busMatrix(busIndex, 12) = thisVoltageMax;  % 电压最大值
+            busMatrix(busIndex, 12) = thisVoltageMax;  % 电压最大值Max Voltage
         else
             busMatrix(busIndex, 12) = 1.5;  % 电压最大值
         end
@@ -112,20 +129,22 @@ for ii = 1 : length(bpa_info)
             while (thisVoltageMin > 2)
                 thisVoltageMin = thisVoltageMin / 10;
             end
-            busMatrix(busIndex, 13) = thisVoltageMin;  % 电压最小值
+            busMatrix(busIndex, 13) = thisVoltageMin;  % 电压最小值 Min voltage
         else
             busMatrix(busIndex, 13) = 0.5;  % 电压最小值
         end
         buslist(busIndex) = {thisline(7 : 14)}; % 母线名称列表
     end
 end
-% 2.2.2. 得到发电机矩阵（11~21列先都赋0）
-disp('获得发电机矩阵...');
+% 2.2.2. 得到发电机矩阵（11~21列先都赋0）Getting generator matrix(0 for column 11-21
+% first)
+disp('获得发电机矩阵...  Getting generator matrix...');
 genMatrix = zeros(genNumber, 21);
 genIndex = 0;
 for ii = 1 : length(bpa_info)
     thisline = bpa_info{ii};
     if (length(thisline) < 65)  % 在长度不足65的字符串后面补足空格
+                                % add space for shorter than 65
         thisline = [thisline, blanks(65 - length(thisline))];
     end
     if (strcmp(thisline(1 : 2), 'BE') || strcmp(thisline(1 : 2), 'BQ') || strcmp(thisline(1 : 2), 'BG') || strcmp(thisline(1 : 2), 'BS'))   % PV节点或平衡节点
@@ -138,11 +157,12 @@ for ii = 1 : length(bpa_info)
             end
         end
         if (~strcmp(thisline(43 : 47), blanks(5)))
-            genMatrix(genIndex, 2) = str2double(thisline(43 : 47));   % 发电机实际有功出力
+            genMatrix(genIndex, 2) = str2double(thisline(43 : 47));   % 发电机实际有功出力Actual Pout
         else
             genMatrix(genIndex, 2) = 0.0;   % 发电机实际有功出力
         end
         genMatrix(genIndex, 3) = 0.0;   % 发电机实际无功出力，对PV节点在潮流计算过程中会修改，这里暂时先填写任意值
+                                        %Actual Qout,meaningless for PV buses
         if (~strcmp(thisline(48 : 52), blanks(5)))
             genMatrix(genIndex, 4) = str2double(thisline(48 : 52));   % 发电机最大无功出力
         else
@@ -173,7 +193,7 @@ for ii = 1 : length(bpa_info)
     end
 end
 % 2.2.3. 得到支路矩阵
-disp('获得支路矩阵...');
+disp('获得支路矩阵...   Getting branch matrix...');
 branchMatrix = zeros(branchNumber, 13);
 branchIndex = 0;
 for ii = 1 : length(bpa_info)
@@ -181,10 +201,10 @@ for ii = 1 : length(bpa_info)
     if (length(thisline) < 72)  % 在长度不足72的字符串后面补足空格
         thisline = [thisline, blanks(72 - length(thisline))];
     end
-    if (strcmp(thisline(1), 'L') || strcmp(thisline(1), 'T'))   % 线路或变压器
+    if (strcmp(thisline(1), 'L') || strcmp(thisline(1), 'T'))   % 线路或变压器line or transformer
         branchIndex = branchIndex + 1;
         busname1 = thisline(7 : 14);
-        for jj = 1 : busNumber    % 支路起始端母线索引
+        for jj = 1 : busNumber    % 支路起始端母线索引from and to buses
             if (strcmp(buslist(jj), busname1))
                 branchMatrix(branchIndex, 2) = jj;
                 break;
@@ -238,6 +258,7 @@ for ii = 1 : length(bpa_info)
             kbase = vbase1 / vbase2;
             kpu = kset / kbase;
             % 修正因BPA中数据的FORTRAN格式造成的读取数据错误
+            % Fixxing FORTRAN-caused dataform problem
             while kpu > 10
                 kpu = kpu / 10;
             end
@@ -258,8 +279,8 @@ for ii = 1 : length(bpa_info)
     end
 end
 % 2.2.4. 得到发电机发电成本矩阵
-disp('获得发电机发电成本矩阵...');
+disp('获得发电机发电成本矩阵...   Getting gencost matrix...');
 gencostMatrix = zeros(genNumber, 7);
 % 2.3. 生成欲返回的电网对象及母线列表
 mpc = struct('version', '2', 'baseMVA', baseMVA, 'bus', busMatrix, 'gen', genMatrix, 'branch', branchMatrix, 'areas', [], 'gencost', gencostMatrix);
-disp(['成功将bpa格式数据文件', bpafile, '转换成matPower数据对象！']);
+disp(['成功将bpa格式数据文件',bpafile, '转换成matPower数据对象！',  'BPA file ', bpafile, ' converted to matPower struct successfully！']);
